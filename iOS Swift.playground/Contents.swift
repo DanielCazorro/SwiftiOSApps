cc87faac18383e46dea0bc3a66fbce2a76f6113d
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-07-17 12:53:05 +0200

    Exercise number 1: PrimeNumbers

--- iOS Swift.playground/Contents.swift
+++ iOS Swift.playground/Contents.swift
@@ -1,3 +1,23 @@
-import Cocoa
+// MARK: - Swift Exercises -
 
-var greeting = "Hello, playground"
+//1 -> Calcula y genera una lista con los 100 primeros números primos y luego un print de los últimos 10
+
+var firstNumber = 2
+var listPrime: [Int] = []
+
+while listPrime.count < 100 {
+    var isPrime = true
+    
+    for number in 2..<firstNumber {
+        if firstNumber % number == 0 {
+            isPrime = false
+        }
+    }
+    if isPrime == true {
+        listPrime.append(firstNumber)
+    }
+    firstNumber += 1
+}
+
+listPrime
+print("The last 10 prime numbers in our list are: \(listPrime.suffix(10))")
