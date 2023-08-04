78b2d2b4b06b2276ee2becb9d6e2e1ce68529408
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-08-04 19:27:16 +0200

    Area of a Triangle

--- iOS Swift.playground/Contents.swift
+++ iOS Swift.playground/Contents.swift
@@ -157,3 +157,29 @@ func calcAge(_ age: Int) -> Int {
 /// Prueba
 
 calcAge(20)
+
+/*
+ Area of a Triangle
+ 
+ Write a function that takes the base and height of a triangle and return its area.
+
+ Examples
+
+ triArea(3, 2) ➞ 3
+
+ triArea(7, 4) ➞ 14
+
+ triArea(10, 10) ➞ 50
+ Notes
+
+ The area of a triangle is: (base * height) / 2
+ Don't forget to return the result.
+ */
+
+func triArea(_ base: Int, _ height: Int) -> Int {
+    return (base * height)/2
+}
+
+/// Prueba
+
+triArea(10, 10)
