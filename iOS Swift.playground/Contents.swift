5eae6887bd34a884649a7593a8a516b1824b044c
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-08-05 21:57:46 +0200

    Find the Perimeter of a Rectangle

--- iOS Swift.playground/Contents.swift
+++ iOS Swift.playground/Contents.swift
@@ -232,3 +232,28 @@ func convert(_ minutes: Int) -> Int {
 
 /// Ejemplo
 convert(5)
+
+/*
+ Find the Perimeter of a Rectangle
+ 
+ Create a function that takes length and width and finds the perimeter of a rectangle.
+
+ Examples
+
+ findPerimeter(6, 7) ➞ 26
+
+ findPerimeter(20, 10) ➞ 60
+
+ findPerimeter(2, 9) ➞ 22
+ Notes
+
+ Don't forget to return the result.
+ */
+
+func findPerimeter(_ length: Int, _ width: Int) -> Int {
+    return (length * 2) + (width * 2)
+}
+
+/// Ejemplo
+
+findPerimeter(20, 10)
