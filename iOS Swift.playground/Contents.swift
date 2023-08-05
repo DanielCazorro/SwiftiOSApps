f8a936f9d8956fc3946f1b8d6907929917c23bb9
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-08-05 19:48:54 +0200

    Return the Next Number from the Integer Passed

--- iOS Swift.playground/Contents.swift
+++ iOS Swift.playground/Contents.swift
@@ -183,3 +183,28 @@ func triArea(_ base: Int, _ height: Int) -> Int {
 /// Prueba
 
 triArea(10, 10)
+
+/*
+ Return the Next Number from the Integer Passed
+ 
+ Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+ Examples
+
+ addition(0) ➞ 1
+
+ addition(9) ➞ 10
+
+ addition(-3) ➞ -2
+ Notes
+
+ Don't forget to return the result.
+ */
+
+func addition(_ num: Int) -> Int {
+    return num + 1
+}
+
+/// Ejemplo
+
+addition(9)
