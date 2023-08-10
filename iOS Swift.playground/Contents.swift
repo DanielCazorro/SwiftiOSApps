5f3d91a059cd3a014267791f660d4f12a232ce8e
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-08-10 10:32:41 +0200

    Swift program compute the sum of the two integers

--- iOS Swift.playground/Contents.swift
+++ iOS Swift.playground/Contents.swift
@@ -459,3 +459,21 @@ func isEqual(_ num1: Int, _ num2: Int) -> Bool {
 }
 /// Ejemplo
 isEqual(36, 36)
+
+
+/*
+ Write a Swift program to compute the sum of the two integers. If the values are equal return the triple their sum.
+ */
+
+func tripleSum (a: Int, b: Int) -> Int {
+    if a == b {
+        return (a+b) * 3
+    } else {
+        return a + b
+    }
+}
+
+/// Ejemplo
+tripleSum(a: 1, b: 2)
+tripleSum(a: 3, b: 2)
+tripleSum(a: 2, b: 2)
