c05b65b9e00d40920d4b492cf75a135a1a266b13
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-02 22:33:51 +0200

    Fixes

---
+++ APP Testing/MVCPatrones/MVCPatrones/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  MVCPatrones
+//
+//  Created by Daniel Cazorro Frías on 2/10/23.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
