70dbb94e22b883c5f55f6673c9e9790c8234c200
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-02 23:02:03 +0200

    CharacterModel con datos

--- APP Testing/MVCPatrones/MVCPatrones/HomeTableViewCell.swift
+++ APP Testing/MVCPatrones/MVCPatrones/HomeTableViewCell.swift
@@ -8,16 +8,34 @@
 import UIKit
 
 class HomeTableViewCell: UITableViewCell {
-
+    
+    // MARK: - IBOUTLETS -
+    
+    @IBOutlet weak var viewHomeCell: UIView!
+    @IBOutlet weak var imageHomeCell: UIImageView!
+    @IBOutlet weak var nameHomeCell: UILabel!
+    
+    
+    // La primera vez, y me prepara la celda
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        viewHomeCell.layer.cornerRadius = 4.0
+        viewHomeCell.layer.shadowColor = UIColor.gray.cgColor
+        viewHomeCell.layer.shadowOffset = .zero
+        viewHomeCell.layer.shadowOpacity = 0.7
+        viewHomeCell.layer.shadowRadius = 4.0
+        
+        imageHomeCell.layer.cornerRadius = 4.0
+        imageHomeCell.layer.opacity = 0.7
     }
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageHomeCell.image = nil
+        nameHomeCell.text = nil
+    }
+    
+    func updateViews() {
+        
+    }
 }
