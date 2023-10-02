4bc932acc349bcd528bb4a67ffda3813f5e75968
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-02 22:21:18 +0200

    Make Navigation to Home View: Working

--- APP Testing/MVCPatrones/MVCPatrones/SplashViewController.swift
+++ APP Testing/MVCPatrones/MVCPatrones/SplashViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class SplashViewController: UIViewController {
 
+    // MARK: - OUTLETS -
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    // MARK: - CICLO DE VIDA -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loadData()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // Cuando vaya aparecer
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if(!activityIndicator.isAnimating) {
+            activityIndicator.startAnimating()
+        }
     }
-    */
+    
+    // Cuando vaya desaparecer
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        activityIndicator.stopAnimating()
+    }
+    
+    // Método Simular carga de Datos
+    private func loadData() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3)) {
+            let nextVC = HomeTableViewController()
+            self.navigationController?.setViewControllers([nextVC], animated: true)
 
+        }
+    }
 }
