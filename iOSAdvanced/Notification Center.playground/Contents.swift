d516958da4d7fcc08b7279a51e7752c727d53298
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-09 22:11:09 +0200

    Two Forms to create NotificationCenterObserver

--- iOSAdvanced/Notification Center.playground/Contents.swift
+++ iOSAdvanced/Notification Center.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+let notificationTest = Notification.Name("TEST_NOTIFICATION")
+
+
+class CashMachine {
+    
+    func money() {
+        NotificationCenter.default.post(name: notificationTest, object: nil)
+    }
+    
+}
+
+class Person {
+    
+    init() {
+        
+        let observer = NotificationCenter.default.addObserver(forName: notificationTest,
+                                               object: nil,
+                                               queue: .main) { notification in
+            print("Notification in forName:object:queue")
+        }
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(onNotificationTest),
+                                               name: notificationTest,
+                                               object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func onNotificationTest(_ notification: Notification) {
+        print("Notification in onNotificationTest()")
+    }
+    
+}
