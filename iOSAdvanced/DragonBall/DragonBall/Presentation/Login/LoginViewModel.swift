f1df1c0b22062a0037e3543b0ca86b087d5c7fb5
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-10 21:49:31 +0200

    Delete Storyboard info.main

--- iOSAdvanced/DragonBall/DragonBall/Presentation/Login/LoginViewModel.swift
+++ iOSAdvanced/DragonBall/DragonBall/Presentation/Login/LoginViewModel.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 
-
 class LoginViewModel: LoginViewControllerDelegate {
     
+    // MARK: - Properties -
+    var viewState: ((LoginViewState) -> Void)?
+    
+    // MARK: - Public functions -
     func onLoginPressed(email: String?, password: String?) {
-        
+        viewState?(.loading(true))
     }
 }
