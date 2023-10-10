137b7cc1e48935e092d0b9712182635832eaaf3b
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-10 20:11:54 +0200

    IBOutlet and IBAction

--- iOSAdvanced/DragonBall/DragonBall/Presentation/Login/LoginViewController.swift
+++ iOSAdvanced/DragonBall/DragonBall/Presentation/Login/LoginViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailFieldError: UILabel!
+    @IBOutlet weak var passwordFieldError: UILabel!
+    
+    @IBAction func onLoginPressed() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
