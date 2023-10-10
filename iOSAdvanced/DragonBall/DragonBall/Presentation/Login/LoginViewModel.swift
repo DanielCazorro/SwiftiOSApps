1b26c2a28e17ec8b344bae0eeeb2b7f7c27cc696
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-10 22:43:58 +0200

    LoginView Model: Email-password

--- iOSAdvanced/DragonBall/DragonBall/Presentation/Login/LoginViewModel.swift
+++ iOSAdvanced/DragonBall/DragonBall/Presentation/Login/LoginViewModel.swift
@@ -15,6 +15,33 @@ class LoginViewModel: LoginViewControllerDelegate {
     // MARK: - Public functions -
     func onLoginPressed(email: String?, password: String?) {
         viewState?(.loading(true))
+        
+        guard isValid(email: email) else {
+            viewState?(.loading(false))
+            viewState?(.showErrorEmail("Indique un email válido"))
+            return
+        }
+        
+        guard isValid(password: password) else {
+            viewState?(.loading(false))
+            viewState?(.showErrorPassword("Indique un password válido"))
+            return
+        }
+        
+        doLoginWith(
+            email: email ?? "",
+            password: password ?? "")
     }
     
+    private func isValid(email: String?) -> Bool {
+        email?.isEmpty == false && email?.contains("@") ?? false
+    }
+    
+    private func isValid(password: String?) -> Bool {
+        password?.isEmpty == false && (password?.count ?? 0) >= 4
+    }
+    
+    private func doLoginWith(email: String, password: String) {
+        
+    }
 }
