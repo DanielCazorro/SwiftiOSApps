d98ad0814b08e11e1f150a95fcfcbdfae74566b3
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-10 20:21:14 +0200

    Creating ViewModel

---
+++ iOSAdvanced/DragonBall/DragonBall/Presentation/Login/LoginViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewModel.swift
+//  DragonBall
+//
+//  Created by Daniel Cazorro Frías on 10/10/23.
+//
+
+import Foundation
+
+
+class LoginViewModel {
+    
+    func onLoginPressed(email: String?, password: String?) {
+        
+    }
+}
