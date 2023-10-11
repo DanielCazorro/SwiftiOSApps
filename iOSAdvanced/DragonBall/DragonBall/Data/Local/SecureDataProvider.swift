1051067ed4aeeb5862dc6721f122b34715304432
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-11 19:47:21 +0200

    Final SecureDataProvider: protocol + functions

--- iOSAdvanced/DragonBall/DragonBall/Data/Local/SecureDataProvider.swift
+++ iOSAdvanced/DragonBall/DragonBall/Data/Local/SecureDataProvider.swift
@@ -8,8 +8,23 @@
 import Foundation
 import KeychainSwift
 
+protocol SecureDataProviderProtocol {
+    func save(token: String)
+    func getToken() -> String?
+}
 
-class KeychainProvider {
+final class SecureDataProvider {
+    private let keychain = KeychainSwift()
     
+    private enum Key {
+        static let token = "KEY_KEYCHAIN_TOKEN"
+    }
     
+    func save(token: String) {
+        keychain.set(token, forKey: Key.token)
+    }
+    
+    func getToken() -> String? {
+        keychain.get(Key.token)
+    }
 }
