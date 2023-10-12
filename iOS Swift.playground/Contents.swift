fafea16aa47f02b19d5093266bdb436ed1776234
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-10-12 17:20:01 +0200

    Exercise 21

--- iOS Swift.playground/Contents.swift
+++ iOS Swift.playground/Contents.swift
@@ -868,3 +868,23 @@ func double_a(_ input: String) -> Bool {
 double_a("abbcaad")
 double_a("ababab")
 double_a("aaaaa")
+
+/*
+21. Write a Swift program to create a string made of every other char starting with the first from a given string. So "Python" will return "Pto"
+*/
+
+func new_string(_ input: String) -> String {
+    var str1 = ""
+    let chars = input.characters
+    
+    for (index, char) in chars.enumerated() {
+        if index % 2 == 0 {
+            str1.append(char)
+        }
+    }
+    
+    return str1
+}
+new_string("Python")
+new_string("Swift")
+new_string("Ab")
