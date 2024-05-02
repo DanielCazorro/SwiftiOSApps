38d89c20cf20d3e581976d6c7ee785021fb6e904
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-05-02 11:29:19 +0200

    Add new Playground page: Vehicle, Car, Motorcycle

---
+++ POO/POO.playground/Pages/Third.xcplaygroundpage/Sources/Car.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public class Car {
+    // MARK: - Public properties
+    public var numberOfDoors: Int
+    
+    // MARK: - Initializers
+    init(numberOfDoors: Int) {
+        self.numberOfDoors = numberOfDoors
+    }
+}
---
+++ POO/POO.playground/Pages/Third.xcplaygroundpage/Sources/Motorcycle.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+
+public class Motorcycle {
+    // MARK: - Public properties
+    public var hasSidecar: Bool
+    
+    // MARK: - Initializers
+    public init(hasSidecar: Bool) {
+        self.hasSidecar = hasSidecar
+    }
+}
