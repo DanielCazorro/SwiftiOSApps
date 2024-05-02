65515c78a351f308d27a91a532220ac8e7d21ae6
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-05-02 12:39:22 +0200

    Update to final class

--- POO/POO.playground/Pages/Third.xcplaygroundpage/Contents.swift
+++ POO/POO.playground/Pages/Third.xcplaygroundpage/Contents.swift
@@ -2,4 +2,24 @@
 import Foundation
 
 let car = Car(numberOfDoors: 4, brand: "Volkswagen", year: 2018)
+let moto = Motorcycle(hasSidecar: false, brand: "Yamaha", year: 2018)
+
 car.startEngine()
+moto.startEngine()
+
+// Cast to Vehicle
+let vehicle = car as Vehicle
+vehicle.brand
+
+// No se puede convertir al contrario:
+// let carVehicle = vehicle as Car
+
+let vehicles: [Vehicle] = [
+    car,
+    moto
+]
+
+print("\n++++BucleFor++++")
+for vehicle in vehicles {
+    print("Brand: \(vehicle.brand) vehicle is started: \(vehicle.isStarted)")
+}
--- POO/POO.playground/Pages/Third.xcplaygroundpage/Sources/Car.swift
+++ POO/POO.playground/Pages/Third.xcplaygroundpage/Sources/Car.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public class Car: Vehicle {
+public final class Car: Vehicle {
     // MARK: - Public properties
     public var numberOfDoors: Int
     
