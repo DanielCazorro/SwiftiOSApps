532f9e774b417f11db5f8f5871ecc1705c54297c
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-05-06 12:55:54 +0200

    Add function to SOLID: single function

--- POO/POO.playground/Pages/Protocols2.0.xcplaygroundpage/Contents.swift
+++ POO/POO.playground/Pages/Protocols2.0.xcplaygroundpage/Contents.swift
@@ -1,30 +1,35 @@
 import Foundation
 
+func manageStep(_ step: any DataCollectingProtocol) {
+    switch step {
+        case let userDataStep as UserData:
+            print("\n++++User Data Step ++++")
+            print("Collected data: \(userDataStep.collectedData)")
+            print("Name: \(userDataStep.collectedData.name), Age: \(userDataStep.collectedData.age)")
+            
+        case let vehicleDataStep as VehicleData:
+            print("\n++++Vehicle Data Step ++++")
+            print("Collected data: \(userDataStep.collectedData)")
+            print("Name: \(vehicleDataStep.collectedData.name), Model: \(vehicleDataStep.collectedData.model)")
+            
+        case let paymentDataStep as PaymentData:
+            print("\n++++Payment Data Step ++++")
+            print("Collected data: \(userDataStep.collectedData)")
+            print("Card number: \(paymentDataStep.collectedData.cardNumber), Expiration date: \(paymentDataStep.collectedData.expirationDate)")
+            
+        default:
+            break
+    }
+}
+
 func runRegistration(formSteps: [any DataCollectingProtocol]) {
-    
     for step in formSteps {
         step.collectData()
     }
     
     print("\nRegistration completed! Here are your data:\n")
     for step in formSteps {
-        switch step {
-        case let userDataStep as UserData:
-            print("\n++++User Data Step ++++")
-            print("Name: \(userDataStep.collectedData.name), Age: \(userDataStep.collectedData.age)")
-            
-        case let vehicleDataStep as VehicleData:
-            print("\n++++Vehicle Data Step ++++")
-            print("Name: \(vehicleDataStep.collectedData.name), Model: \(vehicleDataStep.collectedData.model)")
-            
-        case let paymentDataStep as PaymentData:
-            print("\n++++Payment Data Step ++++")
-            print("Card number: \(paymentDataStep.collectedData.cardNumber), Expiration date: \(paymentDataStep.collectedData.expirationDate)")
-            
-        default:
-            break
-        
-        }
+        manageStep(step)
     }
 }
 
