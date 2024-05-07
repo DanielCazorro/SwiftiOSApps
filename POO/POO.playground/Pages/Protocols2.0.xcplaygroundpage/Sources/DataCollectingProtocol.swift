4d387e938ed398ca69e84f63449bc9e520c57fd5
Daniel Cazorro Frías <leinadhunter10@hotmail.com>
2024-05-07 13:33:45 +0200

    Add other protocol example

--- POO/POO.playground/Pages/Protocols2.0.xcplaygroundpage/Sources/DataCollectingProtocol.swift
+++ POO/POO.playground/Pages/Protocols2.0.xcplaygroundpage/Sources/DataCollectingProtocol.swift
@@ -6,3 +6,28 @@ public protocol DataCollectingProtocol {
     var collecData: DataType { get set }
     func collectData()
 }
+
+protocol ApiDataType {
+    associatedtype Input
+    associatedtype Output
+    
+    func parse(data: Input) -> Output
+}
+
+class User: ApiDataType {
+    typealias Input = String
+    typealias Output = Int
+    
+    func parse(data: Input) -> Output {
+        return 0
+    }
+}
+
+class Contacts: ApiDataType {
+    typealias Input = Float
+    typealias Output = String
+    
+    func parse(data: Float) -> String {
+        return ""
+    }
+}
