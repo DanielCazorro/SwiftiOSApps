3fcd3f3c5542f9b9723041622ba3d36b85cc7aa4
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-05-20 17:36:55 +0200

    Add app Icon and basic logic toogle to change colour

--- iOSBasics/HelloWorld/HelloWorld/ViewController.swift
+++ iOSBasics/HelloWorld/HelloWorld/ViewController.swift
@@ -9,23 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // MARK: - IBOutlets -
+    // MARK: - Properties
+    private var isBlueBackground = false
     
+    // MARK: - IBOutlets
     @IBOutlet weak var helloWorldLabel: UILabel!
     
-    // MARK: - IBActions -
-    
-    @IBAction func didTapButton(_ sender: Any) {
-        print("Hello World")
-        helloWorldLabel.backgroundColor = .blue
-    }
-    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         helloWorldLabel.text = "Hello iOS Developers! 👏🏻"
         helloWorldLabel.backgroundColor = .clear
     }
 
-
+    // MARK: - IBActions
+    @IBAction func didTapButton(_ sender: Any) {
+        changeBackgroundColor()
+    }
+    
+    // MARK: - Private Methods -
+    private func changeBackgroundColor() {
+        isBlueBackground.toggle()
+        
+        updateBacgroundColor()
+        
+        print("Button tapped. Background color is now \(isBlueBackground ? "blue" : "clear")")
+    }
+    
+    private func updateBacgroundColor() {
+        helloWorldLabel.backgroundColor = isBlueBackground ? .blue : .clear
+    }
+    
 }
-
