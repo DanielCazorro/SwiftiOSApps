b7b94e9246b262682414594df16cd11ddab064f9
Daniel Cazorro Frías <danielcazorro@outlook.com>
2024-10-14 18:10:59 +0200

    Add basic tab bar items

--- iOSBasics/HelloWorld/HelloWorld/ThirdViewController.swift
+++ iOSBasics/HelloWorld/HelloWorld/ThirdViewController.swift
@@ -9,15 +9,21 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK: - Segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
-        segue.destination.preferredContentSize = CGSize(width: 300, height:  300)
+        segue.destination.preferredContentSize = CGSize(width: 200, height:  200)
         
         if let presentationController = segue.destination.presentationController {
             presentationController.delegate = self
@@ -25,6 +31,7 @@ class ThirdViewController: UIViewController {
     }
 }
 
+// MARK: - UIPopoverPresentationControllerDelegate
 extension ThirdViewController: UIPopoverPresentationControllerDelegate {
     func adaptivePresentationStyle(for controller: UIPresentationController,
                                    traitCollection: UITraitCollection) -> UIModalPresentationStyle {
