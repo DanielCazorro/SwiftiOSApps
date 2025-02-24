4c6a39aab505eab9b5eb9d7cc1eb06dca36378e2
Daniel Cazorro Frías <danielcazorro@outlook.com>
2025-02-24 22:15:41 +0100

    Add basic structure of HiSwift App

---
+++ iOSBasics/HiWorld/HiSwift/HiSwift/HiSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiSwiftApp.swift
+//  HiSwift
+//
+//  Created by Daniel Cazorro Frías on 24/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HiSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
