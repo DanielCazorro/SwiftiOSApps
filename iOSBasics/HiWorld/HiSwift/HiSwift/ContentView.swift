a1b836e3681eac13df4716f29f37a79d3a376b92
Daniel Cazorro Frías <danielcazorro@outlook.com>
2025-02-26 23:24:40 +0100

    Add launchView and basic structure

--- iOSBasics/HiWorld/HiSwift/HiSwift/ContentView.swift
+++ iOSBasics/HiWorld/HiSwift/HiSwift/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingAlert = false
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(Color.orange)
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.thickMaterial)
+            Text("Hi, world!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.pink)
+                .multilineTextAlignment(.center)
+            Spacer()
+            Button("Tap me") {
+                isShowingAlert.toggle()
+                print(isShowingAlert)
+            }.alert(isPresented: $isShowingAlert) {
+                Alert(title: Text("Title"), message: Text("Message"), dismissButton: .default(Text("Dismiss")))
+            }
         }
         .padding()
     }
