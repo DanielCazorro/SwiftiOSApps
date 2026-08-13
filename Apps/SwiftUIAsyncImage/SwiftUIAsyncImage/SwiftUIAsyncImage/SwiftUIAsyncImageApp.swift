8b0a55ffbac7ae56a4bb1bb1677b97edc27650a3
Daniel Cazorro <leinadhunter10@hotmail.com>
2026-08-13 18:05:19 +0200

    Basic App for testing AsyncImage in SwiftUI

---
+++ Apps/SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Daniel Cazorro on 13/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
