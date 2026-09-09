ef7927c1837cd93d83ce01bdeb099feeb36d2d41
Daniel Cazorro <leinadhunter10@hotmail.com>
2026-09-09 17:25:10 +0200

    Pinch App: Create structure, files and basic home view

---
+++ Apps/PinchApp/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Daniel Cazorro on 08/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/PinchApp/Pinch/Pinch/Screen/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Daniel Cazorro on 08/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - Property
+    @State private var isAnimating = false
+    @State private var imageScale: CGFloat = 1
+
+    // MARK: - Function
+
+    // MARK: - Content
+    var body: some View {
+        NavigationView {
+            ZStack {
+                // MARK: - Page Image
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+
+                // MARK: - 1. TapGesture
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+            }// ZStack
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+        }// Navitation
+        .navigationViewStyle(.stack)
+    }
+}
+#Preview {
+    ContentView()
+        .preferredColorScheme(.dark)
+}
